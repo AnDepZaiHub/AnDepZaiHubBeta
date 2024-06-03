4f50087b9753466528dec7baa01a7d5a74f25c21
AnDepZaiHub <xuanphung0808@gmail.com>
2024-06-03 19:35:59 +0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -1921,7 +1921,7 @@ ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton.Size = UDim2.new(0, 50, 0, 50)
 ImageButton.Draggable = true
-ImageButton.Image = "rbxassetid://17656077747"
+ImageButton.Image = "rbxassetid://17196546025"
 ImageButton.MouseButton1Down:connect(function()
 	game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
 	game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
