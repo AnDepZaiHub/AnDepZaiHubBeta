89789472264d9cbe2e854a1bba2d652e4cbe270a
AnDepZaiHub <xuanphung0808@gmail.com>
2024-10-26 18:49:01 +0700

    Update Hookfunc.lua

--- Hookfunc.lua
+++ Hookfunc.lua
@@ -1,10 +1,3 @@
-task.delay(15, function() 
-    if hookfunction and not islclosure(hookfunction) then 
-        workspace._WorldOrigin.ChildAdded:Connect(function(v)
-            if v.Name == 'DamageCounter' then 
-                v.Enabled = false 
-            end
-        end)
 
         hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function() end)
         hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function() end)
