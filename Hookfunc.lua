214805c430243c7fe127de78eddf20b1fc14a2b5
AnDepZaiHub <xuanphung0808@gmail.com>
2024-11-09 15:00:45 +0700

    Update Hookfunc.lua

--- Hookfunc.lua
+++ Hookfunc.lua
@@ -1,6 +1,15 @@
-
-        hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function() end)
-        hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function() end)
-        hookfunction(require(game:GetService("ReplicatedStorage"):WaitForChild("GuideModule")).ChangeDisplayedNPC, function() end)
-        
-        
+hookfunction(
+    require(game:GetService("ReplicatedStorage").Effect.Container.Death),
+    function()
+    end
+)
+hookfunction(
+    require(game:GetService("ReplicatedStorage").Effect.Container.Respawn),
+    function()
+    end
+)
+hookfunction(
+    require(game:GetService("ReplicatedStorage"):WaitForChild("GuideModule")).ChangeDisplayedNPC,
+    function()
+    end
+)
