318cff93aeebf184e9ecab51cb4627602b1346e5
AnDepZaiHub <xuanphung0808@gmail.com>
2024-12-14 15:46:42 +0700

    Update Hookfunc.lua

--- Hookfunc.lua
+++ Hookfunc.lua
@@ -1,23 +1,11 @@
-hookfunction(
-    require(game:GetService("ReplicatedStorage").Effect.Container.Death),
-    function()
-    end
-)
-hookfunction(
-    require(game:GetService("ReplicatedStorage").Effect.Container.Respawn),
-    function()
-    end
-)
-hookfunction(
-    require(game:GetService("ReplicatedStorage"):WaitForChild("GuideModule")).ChangeDisplayedNPC,
-    function()
-    end
-)
+hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death),function() end)
+hookfunction(require(game:GetService("ReplicatedStorage"):WaitForChild("GuideModule")).ChangeDisplayedNPC,
+function()end)
 
 function gay()
     local lighting = game:GetService("Lighting")
     local lightingLayers = lighting:FindFirstChild("LightingLayers")
-    if lightingLayers then
+    if lightingLayers and game:GetService("Lighting") and game:GetService("Lighting") then
         local darkFog = lightingLayers:FindFirstChild("DarkFog")
         if darkFog then
             darkFog:Destroy()
@@ -25,10 +13,9 @@ function gay()
         end
     end
     local Water = workspace._WorldOrigin["Foam;"]
-    if Water then
+    if Water and workspace._WorldOrigin["Foam;"] then
     Water:Destroy()
     print("Remove Water Effect")
 end        
 end
-
 gay()    
